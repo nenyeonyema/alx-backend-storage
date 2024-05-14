9359dfa763a12e90cd91d338d7142714e1b3e3b7
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-14 13:37:16 -0700

    modified

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,6 +3,7 @@
 Module for listing all documents in a collection.
 """
 
+
 def list_all(mongo_collection):
     """
     Lists all documents in a collection.
@@ -15,11 +16,13 @@ def list_all(mongo_collection):
     """
     return list(mongo_collection.find())
 
+
 if __name__ == "__main__":
     from pymongo import MongoClient
 
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
     schools = list_all(school_collection)
+    print(f"Total documents found: {len(schools)}")
     for school in schools:
         print("[{}] {}".format(school.get('_id'), school.get('name')))
